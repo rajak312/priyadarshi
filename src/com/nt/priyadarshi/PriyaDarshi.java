ad9137d66a02a6f2f8779eeb7f809f74be03a904
Rameshmythri <100982758+Rameshmythri@users.noreply.github.com>
2022-09-08 13:23:58 +0530

    welcome priyadarshi

---
+++ src/com/nt/priyadarshi/PriyaDarshi.java
@@ -0,0 +1,15 @@
+package com.nt.priyadarshi;
+
+public class PriyaDarshi {
+
+	public PriyaDarshi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to the Technology Darshi");
+
+	}
+
+}
